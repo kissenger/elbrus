3a79febee72cbe14998ffd6ec436e2786f6802a6
Gordon Taylor <ivyterrace@hotmail.co.uk>
2020-06-20 18:36:33 +0100

    last changes to front screen, and go!

--- angular/src/app/main/main.component.ts
+++ angular/src/app/main/main.component.ts
@@ -16,17 +16,11 @@ export class MainComponent implements OnInit, OnDestroy {
 
     // gets the url - delay needed to ensure page has loaded - not neat but can't find a better way
     // this.timer = setInterval( () => {
-    //   if (this.router.url !== '/') { 
+    //   if (this.router.url !== '/') {
     //     this.page = this.router.url.split('/')[2];
-    //     clearInterval(this.timer); 
-    //   } 
+    //     clearInterval(this.timer);
+    //   }
     // }, 1);
-
-  }
-
-  ngAfterViewInit() {
-    // console.log(this.router.url);
-    // console.log(window.location.href);
 
   }
 
--- angular/src/app/shared/globals.ts
+++ angular/src/app/shared/globals.ts
@@ -6,11 +6,11 @@ export const M_TO_FT = 3.28084;
 export const EXPORT_FILE_SIZE_LIMIT = 100000;
 export const LONG_PATH_THRESHOLD = 1000;
 
-export const links = {
-    wiki: {
-        elevations: 'https://github.com/kissenger/cotopaxi/wiki/Elevations'
-    }
-};
+// export const links = {
+//     wiki: {
+//         elevations: 'https://github.com/kissenger/cotopaxi/wiki/Elevations'
+//     }
+// };
 
 // the following will eventually be set by user profile
 
