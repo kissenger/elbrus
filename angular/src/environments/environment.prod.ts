2afd788b408c8b008325ad1585732a88fc84cf38
kissenger <ivyterrace@hotmail.co.uk>
2021-01-13 21:42:42 +0000

    fixed again?

--- angular/src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  PROTOCOL: 'https',
-  FRONTEND_URL: 'trailscape.cc',
-  BACKEND_URL: 'trailscape.cc/api',
-  MAPBOX_TOKEN: 'pk.eyJ1Ijoia2lzc2VuZ2VyIiwiYSI6ImNrMWYyaWZldjBtNXYzaHFtb3djaDJobmUifQ.ATRTeTi2mygBXAoXd42KSw',
-  MAPBOX_STYLE_TERRAIN: 'mapbox://styles/kissenger/ckhnuh97p166719qqvqcdih0c',
-  MAPBOX_STYLE_SATELLITE: 'mapbox://styles/kissenger/ckj91j8yn6gqq19l9vo529tix'
-};
