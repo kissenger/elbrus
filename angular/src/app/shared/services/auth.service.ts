733e71cb37dfed47fac65b0a2c57410142a71849
kissenger <ivyterrace@hotmail.co.uk>
2021-02-16 22:36:59 +0000

    fix safari cookie bug

--- angular/src/app/shared/services/auth.service.ts
+++ angular/src/app/shared/services/auth.service.ts
@@ -31,19 +31,19 @@ export class AuthService {
 
 
   public set user(user: TsUser) {
-    document.cookie = `${this.COOKIE_NAME_USER}=${JSON.stringify(user)}; max-age=${this.MAX_AGE}; path=\\`;
+    document.cookie = `${this.COOKIE_NAME_USER}=${JSON.stringify(user)}; max-age=${this.MAX_AGE}; path=/`;
   }
 
   public set guest(user: TsUser) {
-    document.cookie = `${this.COOKIE_NAME_USER}=${JSON.stringify(user)}; path=\\`;
+    document.cookie = `${this.COOKIE_NAME_USER}=${JSON.stringify(user)}; path=/`;
   }
 
   public set userToken(token: string) {
-    document.cookie = `${this.COOKIE_NAME_TOKEN}=${JSON.stringify(token)}; max-age=${this.MAX_AGE}; path=\\`;
+    document.cookie = `${this.COOKIE_NAME_TOKEN}=${JSON.stringify(token)}; max-age=${this.MAX_AGE}; path=/`;
   }
 
   public set guestToken(token: string) {
-    document.cookie = `${this.COOKIE_NAME_TOKEN}=${JSON.stringify(token)}; path=\\`;
+    document.cookie = `${this.COOKIE_NAME_TOKEN}=${JSON.stringify(token)}; path=/`;
   }
 
   public get user() {
@@ -67,7 +67,7 @@ export class AuthService {
   }
 
   private deleteCookie(cookieName: string) {
-    document.cookie = `${cookieName}=; path=\\`;
+    document.cookie = `${cookieName}=; path=/`;
   }
 
   logout() {
