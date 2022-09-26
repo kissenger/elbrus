5b552706f45891c08228bd346fe2598b1960748b
kissenger <ivyterrace@hotmail.co.uk>
2022-09-26 19:10:52 +0100

    deploy script

--- deploy-script.sh
+++ deploy-script.sh
@@ -1,20 +1,24 @@
-echo "deploy-script.sh --> fetching from git"
+# BE CAREFUL UPDATING THIS FILE ON THE VM AND THEN RUNNING IT
+# CHANGES WILL BE OVERWRITTEN BY THE GIT COPY
+RED='\033[0;31m'
+YELLOW_BOLD='\033[1;33m'
+NC='\033[0m'     # No Colour
+echo "${YELLOW_BOLD}deploy-script.sh --> fetching from git${NC}"
 git fetch --all
 git reset --hard origin/master
-echo "deploy-script.sh --> update npm installs"
+echo "${YELLOW_BOLD}deploy-script.sh --> update npm installs${NC}"
 cd /home/ivyterrace/trailscape
 npm install
 cd /home/ivyterrace/trailscape/nodejs
 npm install
-echo "deploy-script.sh --> copy environment files"
+echo "${YELLOW_BOLD}deploy-script.sh --> copy environment files${NC}"
 cp -r /home/ivyterrace/trailscape/env/environments /home/ivyterrace/trailscape/src
 cp /home/ivyterrace/trailscape/env/.env /home/ivyterrace/trailscape/nodejs
-echo "deploy-script.sh --> build angular"
+echo "${YELLOW_BOLD}deploy-script.sh --> build angular${NC}"
 cd /home/ivyterrace/trailscape/angular
 # ng build --prod --base-href /trailscape/
 # ng build --prod
 # fix memory issue per: https://upmostly.com/angular/fixing-angular-build-memory-issues
 npm run build-prod
 cd /home/ivyterrace/trailscape
-echo "deploy-script.sh --> complete"
-echo "==========================================="
+echo "${YELLOW_BOLD}deploy-script.sh --> complete${NC}"
